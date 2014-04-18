4f0a14f0bf01d00520be3e783d146650227c7652
kshantaramanUFL <k.shantaraman@ufl.edu>
2014-04-18 19:31:37 -0400

    Sorting bug fix in search
    Committing Sorting bug fix in search

--- Search.aspx.cs
+++ Search.aspx.cs
@@ -22,49 +22,6 @@ public partial class Search : System.Web.UI.Page
             ViewState["sortOrder"] = "";
             bindGridView("", "");
         }
-        if (Request.QueryString["SearchString"] != null)
-        {
-            string searchString = Request.QueryString["SearchString"].ToUpper();
-            StringBuilder ConnectionString = new StringBuilder();
-            ConnectionString.Append("Provider=").Append(ProjectSettings.dbProvider).Append(";")
-                    .Append(" DATA SOURCE=").Append(ProjectSettings.dbHost).Append(":")
-                    .Append(ProjectSettings.dbPort).Append("/").Append(ProjectSettings.dbSid).Append(";")
-                    .Append("PASSWORD=").Append(ProjectSettings.dbKey).Append(";")
-                    .Append("USER ID=").Append(ProjectSettings.dbUser);
-            string cmd = "SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM (SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM SRAJAGOP.RESTAURANT WHERE UPPER(NAME) LIKE '%" + searchString + "%' OR UPPER(CITY) LIKE '%" + searchString + "%' OR UPPER(STATE) LIKE '%" + searchString + "%' OR UPPER(DESCRIPTION) LIKE '%" + searchString + "%' UNION SELECT DISTINCT SRAJAGOP.RESTAURANT.NAME, DESCRIPTION, OPENTIME, CLOSETIME, ADDRESS1, ADDRESS2, CITY, STATE, ZIP, SRAJAGOP.RESTAURANT.RESTAURANTID FROM SRAJAGOP.RESTAURANT INNER JOIN SRAJAGOP.FDPRICECATALOG ON SRAJAGOP.RESTAURANT.RESTAURANTID = SRAJAGOP.FDPRICECATALOG.RESTAURANTID INNER JOIN SRAJAGOP.FOOD ON SRAJAGOP.FOOD.FOODID = SRAJAGOP.FDPRICECATALOG.FOODID AND SRAJAGOP.FOOD.FOODID IN (SELECT SRAJAGOP.FOOD.FOODID FROM SRAJAGOP.FOOD WHERE (UPPER(SRAJAGOP.FOOD.NAME) LIKE '% " + searchString + " %') OR (UPPER(SRAJAGOP.FOOD.NAME) LIKE '% " + searchString + "') OR (UPPER(SRAJAGOP.FOOD.NAME) LIKE '" + searchString + " %') OR (UPPER(SRAJAGOP.FOOD.NAME) = '" + searchString + "')))";
-            OleDbConnection conn = new OleDbConnection(ConnectionString.ToString());
-            conn.Open();
-            OleDbCommand select_search = new OleDbCommand(cmd, conn);
-            OleDbDataAdapter oAdapter = new OleDbDataAdapter(select_search);
-            tbl.Clear();
-            oAdapter.Fill(tbl);
-            //RestaurantRepeater.DataSource = oReader;
-            //RestaurantRepeater.DataBind();
-            GridView1.DataSource = tbl.Tables[0];
-            GridView1.DataBind();
-            GridView1.PagerSettings.Mode = PagerButtons.Numeric;
-
-            foreach (GridViewRow gr in GridView1.Rows)
-            {
-                HyperLink hp = new HyperLink();
-                hp.Text = gr.Cells[0].Text;
-                rName = hp.Text;
-                rID = gr.Cells[9].Text;
-                rID = "~/Restaurants.aspx?restaurant=" + rID;
-                gr.Cells[0].Controls.Add(hp);
-            }
-            //System.Diagnostics.Debug.WriteLine("Query is " + cmd);
-            ////GridView1.DataSource = oReader;
-            ////GridView1.DataBind();
-            //str += @"<table>";
-            //while (oReader.Read())
-            //{
-            //    str += @"<tr><td><a href='Restaurants.aspx?restaurant=" + oReader[9].ToString() + "'>" + oReader[0].ToString() + @"</a></td><td>" + oReader[1].ToString() + @"</td> <td>" + oReader[2].ToString() + @"</td> <td>" + oReader[3].ToString() + @"</td> <td>" + oReader[4].ToString() + @"</td> <td>" + oReader[5].ToString() + @"</td> <td>" + oReader[6].ToString() + @"</td> <td>" + oReader[7].ToString() + @"</td> <td>" + oReader[8].ToString() + @"</tr>";
-            //}
-            //System.Diagnostics.Debug.WriteLine(str);
-            //str += "</table>";
-            conn.Close();
-        }
     }
 
     protected void Button1_Click(object sender, EventArgs e)
@@ -129,11 +86,12 @@ public partial class Search : System.Web.UI.Page
                     .Append(ProjectSettings.dbPort).Append("/").Append(ProjectSettings.dbSid).Append(";")
                     .Append("PASSWORD=").Append(ProjectSettings.dbKey).Append(";")
                     .Append("USER ID=").Append(ProjectSettings.dbUser);
-            string cmd = "SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM (SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM SRAJAGOP.RESTAURANT WHERE UPPER(NAME) LIKE '%" + searchString + "%' OR UPPER(CITY) LIKE '%" + searchString + "%' OR UPPER(STATE) LIKE '%" + searchString + "%' UNION SELECT DISTINCT SRAJAGOP.RESTAURANT.NAME, DESCRIPTION, OPENTIME, CLOSETIME, ADDRESS1, ADDRESS2, CITY, STATE, ZIP, SRAJAGOP.RESTAURANT.RESTAURANTID FROM SRAJAGOP.RESTAURANT INNER JOIN SRAJAGOP.FDPRICECATALOG ON SRAJAGOP.RESTAURANT.RESTAURANTID = SRAJAGOP.FDPRICECATALOG.RESTAURANTID INNER JOIN SRAJAGOP.FOOD ON SRAJAGOP.FOOD.FOODID = SRAJAGOP.FDPRICECATALOG.FOODID AND UPPER(SRAJAGOP.FOOD.NAME) LIKE '%" + searchString + "%')";
+            string cmd = "SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM (SELECT NAME, DESCRIPTION, OPENTIME, CLOSETIME,ADDRESS1, ADDRESS2, CITY, STATE, ZIP, RESTAURANTID FROM SRAJAGOP.RESTAURANT WHERE UPPER(NAME) LIKE '%" + searchString + "%' OR UPPER(CITY) LIKE '%" + searchString + "%' OR UPPER(STATE) LIKE '%" + searchString + "%' OR UPPER(DESCRIPTION) LIKE '%" + searchString + "%' UNION SELECT DISTINCT SRAJAGOP.RESTAURANT.NAME, DESCRIPTION, OPENTIME, CLOSETIME, ADDRESS1, ADDRESS2, CITY, STATE, ZIP, SRAJAGOP.RESTAURANT.RESTAURANTID FROM SRAJAGOP.RESTAURANT INNER JOIN SRAJAGOP.FDPRICECATALOG ON SRAJAGOP.RESTAURANT.RESTAURANTID = SRAJAGOP.FDPRICECATALOG.RESTAURANTID INNER JOIN SRAJAGOP.FOOD ON SRAJAGOP.FOOD.FOODID = SRAJAGOP.FDPRICECATALOG.FOODID AND SRAJAGOP.FOOD.FOODID IN (SELECT SRAJAGOP.FOOD.FOODID FROM SRAJAGOP.FOOD WHERE (UPPER(SRAJAGOP.FOOD.NAME) LIKE '% " + searchString + " %') OR (UPPER(SRAJAGOP.FOOD.NAME) LIKE '% " + searchString + "') OR (UPPER(SRAJAGOP.FOOD.NAME) LIKE '" + searchString + " %') OR (UPPER(SRAJAGOP.FOOD.NAME) = '" + searchString + "')))";
             OleDbConnection conn = new OleDbConnection(ConnectionString.ToString());
             conn.Open();
             OleDbCommand select_search = new OleDbCommand(cmd, conn);
             OleDbDataAdapter oAdapter = new OleDbDataAdapter(select_search);
+            tbl.Clear();
             oAdapter.Fill(tbl);
             DataView myDataView = new DataView();
             myDataView = tbl.Tables[0].DefaultView;
@@ -144,7 +102,18 @@ public partial class Search : System.Web.UI.Page
 
             GridView1.DataSource = myDataView;
             GridView1.DataBind();
+            GridView1.PagerSettings.Mode = PagerButtons.Numeric;
+
+            foreach (GridViewRow gr in GridView1.Rows)
+            {
+                HyperLink hp = new HyperLink();
+                hp.Text = gr.Cells[0].Text;
+                rName = hp.Text;
+                rID = gr.Cells[9].Text;
+                rID = "~/Restaurants.aspx?restaurant=" + rID;
+                gr.Cells[0].Controls.Add(hp);
+            }            
             conn.Close();
-        }
+        }        
     }
 }
